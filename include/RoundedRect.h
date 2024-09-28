bd0dd98c8db64b8f9d9f9d8628731cac34b3dd87
FrenchieNoah012 <bluearcceo@gmail.com>
2024-09-28 20:56:26 +0930

    Implemented a RoundedRect class to draw rectangle objects without a corner pixel.

---
+++ include/RoundedRect.h
@@ -0,0 +1,26 @@
+#ifndef ROUNDEDRECT_H
+#define ROUNDEDRECT_H
+
+#include "SFML/Graphics.hpp"
+
+#include "Utility.h"
+
+class RoundedRect
+{
+public:
+  RoundedRect(sf::Vector2f centre, sf::Vector2f dim, sf::Color color);
+
+  void SetCentre(sf::Vector2f newCentre);
+  void SetDim(sf::Vector2f newDim);
+
+  void Render(sf::RenderWindow* win) const;
+
+private:
+  sf::Vector2f centre;
+
+  sf::RectangleShape main;
+  sf::RectangleShape left;
+  sf::RectangleShape right;
+};
+
+#endif
